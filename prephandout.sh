b88c28464dd7ed5cbb5b5acab70de2ceb18c97de
MuhamadAjiW <16521119@mahasiswa.itb.ac.id>
2023-10-18 23:57:58 +0700

    feat: stripped and commented activity specific commands

--- prephandout.sh
+++ prephandout.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-ls_module="$(pwd)/labsister-module/usr/local/lib/lab_sister"
-sudo rm -rf "${ls_module}/grader"
-sudo mkdir "${ls_module}/grader"
-sudo rm -rf "${ls_module}/handout/skel"
-sudo mkdir "${ls_module}/handout/skel"
-sudo cp -rf "$(pwd)/datalab-handout/"* "${ls_module}/grader/"
-sudo cp -rf "${ls_module}/grader/bits.c" "${ls_module}/handout/skel/bits.c"
-sudo chmod a+x "${ls_module}/grader/dlc"
+# ls_module="$(pwd)/labsister-module/usr/local/lib/lab_sister"
+# sudo rm -rf "${ls_module}/grader"
+# sudo mkdir "${ls_module}/grader"
+# sudo rm -rf "${ls_module}/handout/skel"
+# sudo mkdir "${ls_module}/handout/skel"
+# sudo cp -rf "$(pwd)/datalab-handout/"* "${ls_module}/grader/"
+# sudo cp -rf "${ls_module}/grader/bits.c" "${ls_module}/handout/skel/bits.c"
+# sudo chmod a+x "${ls_module}/grader/dlc"
